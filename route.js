1aec879c2dca09c6e6caad7d223c6f8239052de6
FabianQF <fquirosf@ucenfotec.ac.cr>
2017-08-07 13:20:11 -0600

    Cambios varios
    Se hacen poco de estilos, se crea el favicon y se coloca una umagen, se enrouta el landing

--- route.js
+++ route.js
@@ -10,6 +10,12 @@
   function configuration($stateProvider, $urlRouterProvider){
     $stateProvider
 
+    .state('landing',{
+      url : '/landing',
+      templateUrl: './components/landing/landing.html',
+      css:'css/style.css'
+    })
+
     .state('players',{
       url : '/players',
       templateUrl: './components/players/players.view.html',
@@ -23,20 +29,33 @@
       css:'css/style.css'
     })
 
+    .state('proprietary',{
+      url : '/proprietary',
+      templateUrl: './components/proprietary/proprietary.view.html',
+      resolve: {
+        load: ['$ocLazyLoad', function($ocLazyLoad){
+          return $ocLazyLoad.load('./components/proprietary/proprietary.controller.js')
+        }]
+      },
+      controller: 'proprietaryController',
+      controllerAs: 'vm',
+      css:'css/style.css'
+    })
+
     .state('buy',{
-  url : '/buy',
-  templateUrl: './components/buy/buy.view.html',
-  resolve: {
-    load: ['$ocLazyLoad', function($ocLazyLoad){
-      return $ocLazyLoad.load('./components/buy/buy.controller.js')
-    }]
-  },
-  controller: 'buyController',
-  controllerAs: 'vm',
-  css:'css/style.css'
-})
+      url : '/buy',
+      templateUrl: './components/buy/buy.view.html',
+      resolve: {
+        load: ['$ocLazyLoad', function($ocLazyLoad){
+          return $ocLazyLoad.load('./components/buy/buy.controller.js')
+        }]
+      },
+      controller: 'buyController',
+      controllerAs: 'vm',
+      css:'css/style.css'
+    })
 
 
-    $urlRouterProvider.otherwise('/players');
+    $urlRouterProvider.otherwise('/landing');
   }//cierre de las rutas
 })();
